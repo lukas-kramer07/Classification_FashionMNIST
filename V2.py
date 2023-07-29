e538f6f44ab4a5a077043fc1473ea32d1281618b
lukas-kramer07 <lukas.kramer7@gmail.com>
2023-07-29 21:25:40 +0200

    V2.py added

---
+++ V2.py
@@ -0,0 +1,98 @@
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.datasets import fashion_mnist
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Create a confusion matrix
+# Note: Adapted from scikit-learn's plot_confusion_matrix()
+
+import itertools
+from sklearn.metrics import confusion_matrix
+
+class_names = ["T-shirt/top","Trousers", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle Boot"]
+def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10,10,), text_size=15):
+  #Create the confusion matrix
+  cm = confusion_matrix(y_true, tf.round(y_preds))
+  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+  n_classes= cm.shape[0]
+
+  # Let's prettify it
+  fig, ax = plt.subplots(figsize=figsize)
+  #Create a matrix plot
+  cax = ax.matshow(cm, cmap=plt.cm.Purples)
+  fig.colorbar(cax)
+
+#set labels to be classes
+  labels = classes if classes else np.arange(cm.shape[0])
+  # Label the axes
+  ax.set(title="Confusion matrix - Fashion_modelV2",
+        xlabel="Predicted label",
+        ylabel="True label",
+        xticks=np.arange(n_classes),
+        yticks=np.arange(n_classes),
+        xticklabels=labels,
+        yticklabels=labels)
+
+  # Set x-labels to the bottom
+  ax.xaxis.set_label_position("bottom")
+  ax.xaxis.tick_bottom()
+
+  #Adjust label size
+  ax.xaxis.label.set_size(text_size)
+  ax.yaxis.label.set_size(text_size)
+  ax.title.set_size(text_size)
+
+  # Set coluor threshhold
+  threshold = (cm.max() + cm.min())/2.
+
+  #Plot the text on each cell
+  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(j, i, f"{cm[i, j]} ({cm_norm[i,j]*100:.1f}%)",
+            horizontalalignment="center",
+            color="white" if cm[i, j] > threshold else "black",
+            size=text_size)
+
+
+
+
+
+
+
+# The data has already been sorted into training and test sets 
+(train_data, train_labels) , (test_data, test_labels) = fashion_mnist.load_data()
+
+
+tf.random.set_seed(42)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Dense(250, activation="relu"),
+    tf.keras.layers.Dense(150, activation="relu"),
+    tf.keras.layers.Dense(250, activation="relu"),
+    tf.keras.layers.Dense(150, activation="relu"),
+    tf.keras.layers.Dense(300, activation="relu"),
+    tf.keras.layers.Dense(10, activation="softmax")
+])
+model.compile(loss="sparse_categorical_crossentropy",
+                 optimizer=tf.keras.optimizers.Adam(),
+                 metrics=["accuracy"])
+bigger_history = model.fit(train_data,
+                          train_labels,
+                          epochs=50,
+                          validation_data=(test_data, test_labels))
+
+
+model.save("Fashion_modelV2")
+
+y_probs = model.predict(test_data)
+y_preds = tf.argmax(y_probs, axis=1)
+
+
+make_confusion_matrix(y_true=test_labels,
+                      y_pred=y_preds,
+                      classes=class_names,
+                      figsize=(13,13),
+                      text_size=8)
+pd.DataFrame(bigger_history.history).plot(title="Fashion_modelV2")
+plt.show()
